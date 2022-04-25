c848c6b8647d3355aa07f176adc34153ab596ff6
guttidani <61143051+guttidani@users.noreply.github.com>
2022-04-25 11:53:38 +0200

    Create BevasarloLista.java

---
+++ src/aft_l4/bevasarlolista/BevasarloLista.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package aft_l4.bevasarlolista;
+
+import java.util.*;
+
+/**
+ *
+ * @author Dani
+ */
+public class BevasarloLista {
+
+    private ArrayList<Termek> termekek = new ArrayList<>();
+    private int vegosszeg;
+
+    public ArrayList<Termek> getTermekek() {
+        return termekek;
+    }
+
+    public void hozzaAd(Termek t) {
+
+    }
+
+    public void torol(int t_id) {
+
+    }
+
+    public ArrayList<Termek> rendezes() {
+        //todo minden meghívásra változik hogy növekvő vagy csökkenő
+        return null;
+    }
+
+    public int getVegosszeg() {
+        return vegosszeg;
+    }
+
+}
