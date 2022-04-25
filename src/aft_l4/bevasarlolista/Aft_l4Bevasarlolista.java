39ee6ea508d80e418b58fd65d582790d5b77acb0
adalinmik <msuda100@gmail.com>
2022-04-25 11:34:35 +0200

    Add project

---
+++ src/aft_l4/bevasarlolista/Aft_l4Bevasarlolista.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package aft_l4.bevasarlolista;
+
+/**
+ *
+ * @author PC
+ */
+public class Aft_l4Bevasarlolista {
+
+    
+}
